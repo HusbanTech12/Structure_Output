0eac01daae905df23c9e7eb710ce5963ce8c5a63
HusbanTech12 <husbantech08@gmail.com>
2025-07-24 23:01:37 +0500

    Structure_Output complete

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
-from agents import Agent , Runner, run 
-from config import config
+from agents import Agent , Runner, function_tool;
+from config import config;
+from pydantic import BaseModel;
 
 
+class WeatherStructure(BaseModel):
+    Location : str
+    Weather : str
+    Temperature : float 
+
+
+
+@function_tool
+def fetch_weather(location : str) -> str:
+    """
+      Fetch Weather the given location.  
+    """
+    return 'The Weather in {location} is Sunny with a temperature at 25°C'   
+    
+    
+    
 agent = Agent(
     name = 'Helpful Assistant',
-    instructions='You are a Helpful Assistant.'
+    instructions='You are a Helpful Assistant.',
+    tools = [fetch_weather],
+    output_type= WeatherStructure
 )
  
  
 result = Runner.run_sync(
     agent,
-    input = 'Hi, How are you ?',
+    input = 'What is the weather in karachi ?',
     run_config=config
 )    
-
-print(result.final_output)
+# print(agent.as_tool)
+print(result.final_output)  
